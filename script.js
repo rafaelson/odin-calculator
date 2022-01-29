d9b34101db6b5ca4291ecbac76750cd78923e177
Rafael Nelson <rafaelson1337@gmail.com>
2022-01-29 01:33:40 -0300

    testing functions

--- script.js
+++ script.js
@@ -1,8 +1,74 @@
-document.querySelectorAll(".button").forEach((elem) => {
+let operation;
+let value1 = null;
+let value2 = null;
+const displayBot = document.querySelector(".display-bot");
+
+document.querySelectorAll(".number").forEach((elem) => {
   elem.addEventListener("click", () => {
-    console.log(elem.textContent);
-    display_bot.textContent += elem.textContent;
+    //console.log(elem.textContent);
+    displayBot.textContent += elem.textContent;
   });
 });
 
-const display_bot = document.querySelector(".display-bot");
+document.querySelectorAll(".operator").forEach((elem) => {
+  elem.addEventListener("click", () => {
+    if (value1 == null) {
+      value1 = Number(displayBot.textContent);
+      if (operation != "=") {
+        operation = elem.textContent;
+      } else {
+        resetValues();
+      }
+      cleanDisplay();
+    } else {
+      value2 = Number(displayBot.textContent);
+      cleanDisplay();
+    }
+    if (value1 != null && value2 != null && elem.textContent == "=") {
+      selectOperation(operation);
+    }
+  });
+});
+
+function cleanDisplay() {
+  displayBot.textContent = "";
+}
+
+function resetValues() {
+  value1 = null;
+  value2 = null;
+}
+
+function selectOperation(op) {
+  switch (op) {
+    case "+":
+      displayBot.textContent = add();
+
+      break;
+    case "-":
+      break;
+    case "×":
+      break;
+    case "÷":
+      break;
+    case "=":
+      break;
+  }
+  resetValues();
+}
+
+function add() {
+  return value1 + value2;
+}
+
+function sub() {
+  return value1 - value2;
+}
+
+function mul() {
+  return value1 * value2;
+}
+
+function div() {
+  return value1 / value2;
+}
